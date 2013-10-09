a36cf5f673453840611c420947d330bb701db718
majo <majo63@email.cz>
2013-10-09 17:46:02 +0200

    Add enum CustomerStatus; implement Customer entity

--- src/main/java/cz/muni/fi/pa165/jtravelagency/jtravelagency/Customer.java
+++ src/main/java/cz/muni/fi/pa165/jtravelagency/jtravelagency/Customer.java
@@ -4,17 +4,39 @@
  */
 package cz.muni.fi.pa165.jtravelagency.jtravelagency;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 /**
  *
- * @author jakub
+ * @author Marian Varaga
  */
-public class Customer {
-    
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    
+    @Column(name = "first_name")
     private String firstName;
-    
+    @Column(name = "last_name")
     private String lastName;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private CustomerStatus status;
+    @OneToMany(mappedBy = "customer")
+    private List<Reservation> reservations;
+
+    public Customer() {
+    }
 
     /**
      * @return the id
@@ -50,7 +72,42 @@ public class Customer {
     public String getLastName() {
         return lastName;
     }
-    
-    
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    public CustomerStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CustomerStatus status) {
+        this.status = status;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Customer other = (Customer) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 }
