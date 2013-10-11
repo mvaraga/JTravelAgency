90ec32fad1e4fdc85a7e587511afab4e169f0b96
mvaraga <marian.varaga@gmail.com>
2013-10-11 18:19:31 +0200

    add methods equals and hascode to class Excursion

--- src/main/java/cz/muni/fi/pa165/jtravelagency/jtravelagency/Excursion.java
+++ src/main/java/cz/muni/fi/pa165/jtravelagency/jtravelagency/Excursion.java
@@ -7,6 +7,7 @@ package cz.muni.fi.pa165.jtravelagency.jtravelagency;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -106,6 +107,28 @@ public class Excursion implements Serializable {
      */
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Excursion other = (Excursion) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
     }
     
 }
