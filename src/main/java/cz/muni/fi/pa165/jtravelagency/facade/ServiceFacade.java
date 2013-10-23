4507e1d9bb9a4d867cae09a8ef910f9f964f743e
xmarecek <kuba.marecek@gmail.com>
2013-10-23 16:45:50 +0200

    Updated project structure

---
+++ src/main/java/cz/muni/fi/pa165/jtravelagency/facade/ServiceFacade.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.jtravelagency.facade;
+
+/**
+ *
+ * @author jakub
+ */
+public interface ServiceFacade {
+    
+}
---
+++ src/main/java/cz/muni/fi/pa165/jtravelagency/service/ExcrursionService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.jtravelagency.service;
+
+/**
+ *
+ * @author jakub
+ */
+public interface ExcrursionService {
+    
+}
