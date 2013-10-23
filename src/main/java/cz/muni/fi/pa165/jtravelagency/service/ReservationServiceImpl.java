633c5b58654c83a277424de45387eb8d9f640e6e
rpovalova <radka.povalova@gmail.com>
2013-10-23 17:42:16 +0200

    reservation.entity

--- src/main/java/cz/muni/fi/pa165/jtravelagency/service/ReservationServiceImpl.java
+++ src/main/java/cz/muni/fi/pa165/jtravelagency/service/ReservationServiceImpl.java
@@ -4,10 +4,55 @@
  */
 package cz.muni.fi.pa165.jtravelagency.service;
 
+import cz.muni.fi.pa165.jtravelagency.dao.ReservationDAO;
+import cz.muni.fi.pa165.jtravelagency.dto.CustomerDTO;
+import cz.muni.fi.pa165.jtravelagency.dto.ReservationDTO;
+import cz.muni.fi.pa165.jtravelagency.dto.TripDTO;
+import cz.muni.fi.pa165.jtravelagency.entity.Reservation;
+import java.util.List;
+
 /**
  *
  * @author jakub
  */
-public class ReservationServiceImpl {
+public class ReservationServiceImpl implements ReservationService {
+    
+    private ReservationDAO reservationDAO;
+
+    public void setReservationDAO(ReservationDAO reservationDAO) {
+        this.reservationDAO = reservationDAO;
+    }
+    
+    public void create(ReservationDTO reservation){}
+    public void delete(ReservationDTO  reservation){}
+    public void update(ReservationDTO  reservation){}
+    public ReservationDTO  get(Long id){
+        return null;
+    }
+    public List<ReservationDTO> getAll(){
+        return null;
+    }
+    public List<ReservationDTO> getByTrip(TripDTO trip){
+        return null;
+    }
+    public List<ReservationDTO> getByCustomer(CustomerDTO customer){
+        return null;
+    }
+    
+    private static Reservation DTOreservationtoEntity(ReservationDTO reservationDTO){
+        if (reservationDTO ==null) {return null;}
+        
+        Reservation reservation = new Reservation();
+        reservation.setId(reservationDTO.getId());
+        reservation.setTrip(reservationDTO.getTrip());
+        reservation.setExcursions(reservationDTO.getExcursions());
+        
+        return null;
+  
+    }
+    
+    private static ReservationDTO ReservationtoDTO(){
+        return null;
+    }
     
 }
