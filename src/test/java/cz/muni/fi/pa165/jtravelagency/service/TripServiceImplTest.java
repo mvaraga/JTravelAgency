eb13a0f5b67093878252792df334498b99de44b4
rpovalova <radka.povalova@gmail.com>
2013-10-30 21:42:20 +0100

    tripservice test first part

--- src/test/java/cz/muni/fi/pa165/jtravelagency/service/TripServiceImplTest.java
+++ src/test/java/cz/muni/fi/pa165/jtravelagency/service/TripServiceImplTest.java
@@ -9,14 +9,17 @@ import cz.muni.fi.pa165.jtravelagency.dao.TripDAOImpl;
 import cz.muni.fi.pa165.jtravelagency.dto.ExcursionDTO;
 import cz.muni.fi.pa165.jtravelagency.dto.TripDTO;
 import cz.muni.fi.pa165.jtravelagency.entity.Trip;
+import cz.muni.fi.pa165.jtravelagency.util.DTOAndDAOMapper;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
 import org.joda.time.LocalDate;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
 /**
@@ -50,20 +53,31 @@ public class TripServiceImplTest extends TestCase {
      * Test of setTripDAO method, of class TripServiceImpl.
      */
     public void testSetTripDAO() {
-           TripDTO tripDTO=newTripDTO();
-
     }
 
     /**
      * Test of create method, of class TripServiceImpl.
      */
     public void testCreate() {
-        System.out.println("create");
-        TripDTO tripDTO = null;
-        TripServiceImpl instance = new TripServiceImpl();
-        instance.create(tripDTO);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        doThrow(new IllegalArgumentException()).when(tripDao).createTrip(null);
+        
+        try{
+            tripService.create(null);
+            fail();
+        }catch(IllegalArgumentException ex){
+            
+        }
+         
+        verify(tripDao).createTrip(null);
+        verify(tripDao,never()).updateTrip(null);
+        verifyNoMoreInteractions(tripDao);
+                
+        Trip trip = prepareTrip();
+        
+        tripService.create(DTOAndDAOMapper.entityToDto(trip));
+        
+        verify(tripDao,times(1)).createTrip(trip);
+        verify(tripDao,times(0)).updateTrip(trip);
     }
 
     /**
@@ -84,37 +98,88 @@ public class TripServiceImplTest extends TestCase {
      * Test of update method, of class TripServiceImpl.
      */
     public void testUpdate() {
-        System.out.println("update");
-        TripDTO trip = null;
-        TripServiceImpl instance = new TripServiceImpl();
-        instance.update(trip);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        doThrow(new IllegalArgumentException()).when(tripDao).updateTrip(null);
+        
+        try{
+            tripService.update(null);
+            fail();
+        }catch(IllegalArgumentException ex){
+           
+        }
+         
+        verify(tripDao,never()).createTrip(null);
+        verify(tripDao,times(1)).updateTrip(null);
+        verifyNoMoreInteractions(tripDao);
+                
+        Trip trip = prepareTrip();
+        tripService.update(DTOAndDAOMapper.entityToDto(trip));
+        
+        verify(tripDao,times(1)).updateTrip(trip);
+        verify(tripDao,times(0)).createTrip(trip);
     }
 
     /**
      * Test of delete method, of class TripServiceImpl.
      */
     public void testDelete() {
-        System.out.println("delete");
-        TripDTO trip = null;
-        TripServiceImpl instance = new TripServiceImpl();
-        instance.delete(trip);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+     doThrow(new IllegalArgumentException()).when(tripDao).deleteTrip(null);
+        
+        try{
+            tripService.delete(null);
+            fail();
+        }catch(IllegalArgumentException ex){
+            
+        }
+         
+        verify(tripDao,never()).createTrip(null);
+        verify(tripDao,times(1)).deleteTrip(null);
+        verify(tripDao,never()).updateTrip(null);
+        verifyNoMoreInteractions(tripDao);
+                
+        Trip trip = prepareTrip();
+        tripService.delete(DTOAndDAOMapper.entityToDto(trip));
+        
+        verify(tripDao,times(1)).deleteTrip(trip);
+        verify(tripDao,times(0)).createTrip(trip);
+        verify(tripDao,never()).updateTrip(trip);
     }
 
     /**
      * Test of getAll method, of class TripServiceImpl.
      */
     public void testGetAll() {
-        System.out.println("getAll");
-        TripServiceImpl instance = new TripServiceImpl();
-        List expResult = null;
-        List result = instance.getAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        doThrow(new IllegalArgumentException()).when(tripDao).getTrip(null);
+        doThrow(new IllegalArgumentException()).when(tripDao).getTrip(-1l);
+        
+        try{
+            tripService.get(null);
+            fail();
+        }catch(IllegalArgumentException ex){
+          
+        }
+        
+        try{
+            tripService.get(-1l);
+            fail();
+        }catch(IllegalArgumentException ex){
+            
+        }
+         
+        verify(tripDao,never()).createTrip(null);
+        verify(tripDao,times(1)).getTrip(null);
+        verify(tripDao,never()).updateTrip(null);
+        verify(tripDao,times(1)).getTrip(-1l);
+                
+        Trip trip = prepareTrip();
+        trip.setId(1l);
+        
+        when(tripDao.getTrip(1l)).thenReturn(trip); 
+            
+        assertEquals(trip, tripService.get(trip.getId()));
+        
+        verify(tripDao,times(1)).getTrip(1l);
+        verify(tripDao,times(0)).createTrip(trip);
+        verify(tripDao,never()).updateTrip(trip);
     }
 
     /**
@@ -146,18 +211,24 @@ public class TripServiceImplTest extends TestCase {
         fail("The test case is a prototype.");
     }
     
-        private TripDTO newTripDTO() {
-        TripDTO tripDTO = new TripDTO();
-        tripDTO.setDateFrom(new LocalDate(2013, 11, 23));
-        tripDTO.setDateTo(new LocalDate(2013, 1, 30));
-        tripDTO.setDestination("Spain");
-        tripDTO.setAvailableTrips(10);
-        tripDTO.setPrice(new BigDecimal(15200.25));
+        private Trip prepareTrip() {
+        Trip preparedTrip = new Trip();
+        preparedTrip.setDateFrom(new LocalDate(2013, 11, 23));
+        preparedTrip.setDateTo(new LocalDate(2013, 1, 30));
+        preparedTrip.setDestination("Spain");
+        preparedTrip.setAvailableTrips(10);
+        preparedTrip.setPrice(new BigDecimal(15200.25));
         
-        //List<ExcursionDTO> excursions=new ArrayList<ExcursionDTO>();
-        //excursions.add(new ExcursionDTO());
-        //excursions.add(new ExcursionDTO());
-        return tripDTO;
-}
+        return preparedTrip;
+    }
+    
+    private static void assertTripDeepEquals(Trip expected, Trip actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getDateFrom(), actual.getDateFrom());
+        assertEquals(expected.getDateTo(), actual.getDateTo());
+        assertEquals(expected.getDestination(), actual.getDestination());
+        assertEquals(expected.getAvailableTrips(), actual.getAvailableTrips());
+        assertEquals(expected.getPrice(), actual.getPrice());
+    }
 
 }
