2c9317f0d2d3ff1c3f130cef0688be31c6b984cf
mvaraga <marian.varaga@gmail.com>
2013-11-19 21:18:46 +0100

    add CustomersActionBean

---
+++ JTravelAgency-web/src/main/java/cz/muni/fi/pa165/jtravelagency/web/CustomersActionBean.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.jtravelagency.web;
+
+import cz.muni.fi.pa165.jtravelagency.dto.CustomerDTO;
+import cz.muni.fi.pa165.jtravelagency.facade.ServiceFacade;
+import java.util.List;
+import net.sourceforge.stripes.action.Before;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.LocalizableMessage;
+import net.sourceforge.stripes.action.RedirectResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.controller.LifecycleStage;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+import net.sourceforge.stripes.validation.ValidationErrorHandler;
+import net.sourceforge.stripes.validation.ValidationErrors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author xvaraga
+ */
+public class CustomersActionBean extends BaseActionBean implements ValidationErrorHandler {
+final static Logger log = LoggerFactory.getLogger(CustomersActionBean.class);
+
+    @SpringBean //Spring can inject even to private and protected fields
+    protected ServiceFacade facade;
+
+    //--- part for showing a list of books ----
+    private List<CustomerDTO> customers;
+
+    @DefaultHandler
+    public Resolution list() {
+        log.debug("list()");
+        customers = facade.getAllCustomers();
+        return new ForwardResolution("/customer/list.jsp");
+    }
+
+    public List<CustomerDTO> getCustomers() {
+        return customers;
+    }
+
+    //--- part for adding a book ----
+
+//    @ValidateNestedProperties(value = {
+//            @Validate(on = {"add", "save"}, field = "author", required = true),
+//            @Validate(on = {"add", "save"}, field = "title", required = true),
+//            @Validate(on = {"add", "save"}, field = "year", required = true, minvalue = 800)
+//    })
+    private CustomerDTO customer;
+
+    public Resolution add() {
+        log.debug("add() customer={}", customer);
+        facade.createCustomer(customer);
+        getContext().getMessages().add(new LocalizableMessage("customer.add.message",escapeHTML(customer.getFirstName()),escapeHTML(customer.getLastName())));
+        return new RedirectResolution(this.getClass(), "list");
+    }
+
+    @Override
+    public Resolution handleValidationErrors(ValidationErrors errors) throws Exception {
+        //fill up the data for the table if validation errors occured
+        customers = facade.getAllCustomers();
+        //return null to let the event handling continue
+        return null;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    //--- part for deleting a book ----
+
+    public Resolution delete() {
+        log.debug("delete({})", customer.getId());
+        //only id is filled by the form
+        customer = facade.getCustomer(customer.getId());
+        facade.deleteCustomer(customer);
+        getContext().getMessages().add(new LocalizableMessage("customer.delete.message",escapeHTML(customer.getFirstName()),escapeHTML(customer.getLastName())));
+        return new RedirectResolution(this.getClass(), "list");
+    }
+
+    //--- part for editing a book ----
+
+    @Before(stages = LifecycleStage.BindingAndValidation, on = {"edit", "save"})
+    public void loadCustomerFromDatabase() {
+        String ids = getContext().getRequest().getParameter("customer.id");
+        if (ids == null) return;
+        customer = facade.getCustomer(Long.parseLong(ids));
+    }
+
+    public Resolution edit() {
+        log.debug("edit() customer={}", customer);
+        return new ForwardResolution("/customer/edit.jsp");
+    }
+
+    public Resolution save() {
+        log.debug("save() customer={}", customer);
+        facade.updateCustomer(customer);
+        return new RedirectResolution(this.getClass(), "list");
+    }
+}
+
+    
+    
+
