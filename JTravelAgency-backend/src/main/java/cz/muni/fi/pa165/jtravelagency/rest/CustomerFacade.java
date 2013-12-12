91d48d17fe3fbaece2fb23fc9aee5e827db7ff35
mvaraga <marian.varaga@gmail.com>
2013-12-12 16:17:11 +0100

    allow call from external server; move client file to client module; add tomcat plugin to client

--- JTravelAgency-backend/src/main/java/cz/muni/fi/pa165/jtravelagency/rest/CustomerFacade.java
+++ JTravelAgency-backend/src/main/java/cz/muni/fi/pa165/jtravelagency/rest/CustomerFacade.java
@@ -12,6 +12,8 @@ import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,6 +22,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -31,19 +34,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Path("customers")
 @Singleton
 public class CustomerFacade {
+
     private static ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
     private ServiceFacade facade = applicationContext.getBean("facade", ServiceFacade.class);
     //private static List<CustomerDTO> customers = new ArrayList<>();
-    
+    private String _corsHeaders;
+
+    private Response makeCORS(ResponseBuilder req, String returnMethod) {
+        ResponseBuilder rb = req.header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+        if (!"".equals(returnMethod)) {
+            rb.header("Access-Control-Allow-Headers", returnMethod);
+        }
+
+        return rb.build();
+    }
+
+    private Response makeCORS(ResponseBuilder req) {
+        return makeCORS(req, _corsHeaders);
+    }
+
     @Context
     private UriInfo context;
-    
+
     public CustomerFacade() {
         //customers.clear();
 //        List<CustomerDTO> customers = facade.getAllCustomers();
 //        this.customers.addAll(customers);
     }
-    
+
     @GET
     @Produces("text/plain")
     public String getPlain() {
@@ -55,25 +75,25 @@ public class CustomerFacade {
             returnString.append(customer.getLastName());
             returnString.append("\n");
         }
- 
+
         return returnString.toString();
     }
-    
+
     @Path("{id}")
     public CustomerDTO getCustomerResource(@PathParam("id") Long id) {
         return facade.getCustomer(id);
     }
-    
+
     @GET
     @Path("json/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public CustomerDTO getJson(@PathParam("id") Long id) {
-        if (facade.getCustomer(id)==null){
-             throw new WebApplicationException(Response.Status.NOT_FOUND);
+        if (facade.getCustomer(id) == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         return facade.getCustomer(id);
     }
- 
+
     @GET
     @Path("count")
     @Produces(MediaType.TEXT_PLAIN)
@@ -81,24 +101,29 @@ public class CustomerFacade {
         List<CustomerDTO> customers = facade.getAllCustomers();
         return String.valueOf(customers.size());
     }
-    
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response postJson(CustomerDTO customerResource) {
         facade.createCustomer(customerResource);
         System.out.println("Created customer " + customerResource.getId());
-        return Response.created(URI.create(context.getAbsolutePath() + "/"+ customerResource.getId())).build();
+        return Response.created(URI.create(context.getAbsolutePath() + "/" + customerResource.getId())).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, OPTIONS").build();
     }
-    
+
+    @OPTIONS
+    public Response corsMyResource(@HeaderParam("Access-Control-Request-Headers") String requestH) {
+        _corsHeaders = requestH;
+        return makeCORS(Response.ok(), requestH);
+    }
+
     @DELETE
     @Path("{id}")
     public void delete(@PathParam("id") Long id) {
         System.out.println("---- Deleting item nr. " + id);
-        
-        if (facade.getCustomer(id)==null){
-             throw new WebApplicationException(Response.Status.NOT_FOUND);
+
+        if (facade.getCustomer(id) == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         facade.deleteCustomer(facade.getCustomer(id));
     }
